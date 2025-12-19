6f086afecd0eaad5bed0fc6320c10c9a53f26d7d
Koushik <koushik@OBAIDs-MacBook-Pro-2.local>
2025-12-19 08:51:00 +0400

    removed init

--- init.py
+++
@@ -1,9 +0,0 @@
-import dagshub
-dagshub.init(repo_owner='sripathikoushik244',
-             repo_name='MLProject-mlflow',
-             mlflow=True)
-
-import mlflow
-with mlflow.start_run():
-  mlflow.log_param('parameter name', 'value')
-  mlflow.log_metric('metric name', 1)
